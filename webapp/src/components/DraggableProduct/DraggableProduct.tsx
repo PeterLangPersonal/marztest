1c01fd7daf144570dab33ec9859391c05506e99f
Peter Lang <peterlang1999@gmail.com>
2023-08-10 16:32:15 -0400

    patch: added missing components to item

--- webapp/src/components/DraggableProduct/DraggableProduct.tsx
+++ webapp/src/components/DraggableProduct/DraggableProduct.tsx
@@ -11,6 +11,9 @@ const DraggableItem = (props: DraggableProductProps) => (
         className='bg-neutral-300 flex items-center justify-between mt-1 p-3 rounded w-full'
         data-testid={`draggable-container-${props.item.ProductID}`}
     >
+        <span data-testid={`draggable-productID-${props.item.ProductID}`}>{ props.item.ProductID }</span>
+        <span data-testid={`draggable-productName-${props.item.ProductID}`}>{ props.item.ProductName }</span>
+        <img src={props.item.ProductPhotoURL} alt={`${props.item.ProductName}`}/>
         {(() => {
             const { ProductID, ProductStatus } = props.item;
             return (
